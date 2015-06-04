e84959459d91e2d185f2712bfa52aa3d72a2b613
unknown <luis.rodriguez@desarrollo.sigmamovil.local>
2015-06-04 16:56:39 -0500

    Ajustando Mapa del Historial

--- app/controllers/VisitController.php
+++ app/controllers/VisitController.php
@@ -2,6 +2,10 @@
 
 class VisitController extends ControllerBase
 {
+    public function indexAction()
+    {
+        
+    }
     public function getrowsAction()
     {
         $post = $this->request->getPost('paginator');
@@ -53,8 +57,15 @@ class VisitController extends ControllerBase
     
     public function maphistoryAction($idUser)
     {
-        $visits = Account::findFirst(array(
-            "conditions" => "idAccount = ?1",
+        $visits = Visit::findFirst(array(
+            "conditions" => "idUser = ?1",
+            "bind" => array(1 => $idUser)
+        ));
+        
+        
+        
+        $user = User::findFirst(array(
+            "conditions" => "idUser = ?1",
             "bind" => array(1 => $idUser)
         ));
         
@@ -63,6 +74,7 @@ class VisitController extends ControllerBase
             return $this->response->redirect('visit/index');
         }
         
-        
+        $this->view->setVar('visits', $visits);
+        $this->view->setVar('user', $user);
     }
 }
