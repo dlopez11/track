38f09ca3dfad66ba2d4d1831e8624afc80ab4a86
unknown <luis.rodriguez@desarrollo.sigmamovil.local>
2015-06-04 12:25:21 -0500

    CRUD Accounts Completed....

--- app/models/Account.php
+++ app/models/Account.php
@@ -1,5 +1,8 @@
 <?php
 
+use Phalcon\Mvc\Model\Validator\Uniqueness;
+use Phalcon\Mvc\Model\Validator\PresenceOf;
+
 class Account extends \Phalcon\Mvc\Model
 
 {    
@@ -8,5 +11,67 @@ class Account extends \Phalcon\Mvc\Model
     public function initialize()
     {
         $this->hasMany("idAccount", "User", "idAccount");
+    }
+    
+    public function validation()
+    {
+        $this->validate(new PresenceOf(array(
+            'field' => 'name',
+            'message' => 'El nombre es obligatorio, por favor valide la información'
+        )));
+        
+        $this->validate(new SpaceValidator(array(
+            'field' => 'name',
+            'message' => 'El campo nombre esta vacío, por favor valide la información'
+        )));
+        
+        $this->validate(new Uniqueness(array(
+            'field' => 'name',
+            'message' => 'Ya existe una cuenta con este nombre, por favor valide la información'
+        )));
+        
+        $this->validate(new PresenceOf(array(
+            'field' => 'nit',
+            'message' => 'El nombre es obligatorio, por favor valide la información'
+        )));
+        
+        $this->validate(new SpaceValidator(array(
+            'field' => 'nit',
+            'message' => 'El campo nombre esta vacío, por favor valide la información'
+        )));
+        
+        $this->validate(new PresenceOf(array(
+            'field' => 'address',
+            'message' => 'La dirección es obligatoria, por favor valide la información'
+        )));
+        
+        $this->validate(new SpaceValidator(array(
+            'field' => 'address',
+            'message' => 'El campo dirección esta vacío, por favor valide la información'
+        )));
+        
+        $this->validate(new PresenceOf(array(
+            'field' => 'city',
+            'message' => 'El campo ciudad es obligatorio, por favor valide la información'
+        )));
+        
+        $this->validate(new SpaceValidator(array(
+            'field' => 'city',
+            'message' => 'El campo ciudad esta vacío, por favor valide la información'
+        )));
+        
+        $this->validate(new PresenceOf(array(
+            'field' => 'state',
+            'message' => 'El campo departamento es obligatorio, por favor valide la información'
+        )));
+        
+        $this->validate(new SpaceValidator(array(
+            'field' => 'state',
+            'message' => 'El campo departamento esta vacío, por favor valide la información'
+        )));
+        
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
     }
 }
