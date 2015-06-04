dc0f773942f0929a8cde09df507eba096e471f30
unknown <william.montiel@liderdesarrollo.sigmamovil.local>
2015-06-04 16:42:00 -0500

    More details

--- public/js/dom-data-manager.js
+++ public/js/dom-data-manager.js
@@ -39,13 +39,8 @@ DomManager.prototype.createTable = function() {
 };
 
 DomManager.prototype.refreshTable = function() {
+    this.content.find('#content').hide('slow');
     this.content.find('#content').empty(); 
-//    var self = this;
-//    this.content.find('#content').animate({
-//        opacity: 0.25
-//    }, 500, function() {
-//        self.content.find('#content').empty(); 
-//    });
     
     for (var i = 0; i < this.rows.length; i++) {
         var row = $('<tr>\n\
@@ -54,9 +49,11 @@ DomManager.prototype.refreshTable = function() {
                         <td>' + this.rows[i].visit + '</td>\n\
                         <td>' + this.rows[i].client + '</td>\n\
                         <td>' + this.rows[i].battery + '%</td>\n\
-                        <td><a href="' + url + '/map">' + this.rows[i].location + '</a></td>\n\
+                        <td><a href="' + url + '/map/' + this.rows[i].idUser + '">' + this.rows[i].location + '</a></td>\n\
                     </tr>');
         
+        
         this.content.find('#content').append(row);
+        this.content.find('#content').show('slow');
     }
 };
