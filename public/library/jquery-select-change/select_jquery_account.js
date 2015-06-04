5b15629617b851001b1eb8e5a5ef0b75660d59da
unknown <luis.rodriguez@desarrollo.sigmamovil.local>
2015-06-04 11:13:45 -0500

    Ajustes y mas ciudades

--- public/library/jquery-select-change/select_jquery_account.js
+++ public/library/jquery-select-change/select_jquery_account.js
@@ -38,6 +38,45 @@ var CALDAS = [
     {display: "CHINCHINA", value: "CHINCHINA" }
 ];
 
+var CAQUETA = [
+    {display: "FLORENCIA", value: "FLORENCIA" }
+];
+
+var CASANARE = [
+    {display: "YOPAL", value: "YOPAL" }
+];
+
+var CAUCA = [
+    {display: "POPAYAN", value: "POPAYAN" },
+    {display: "SANTANDER DE QUILICHAO", value: "SANTANDER DE QUILICHAO" }
+];
+
+var CESAR = [
+    {display: "VALLEDUPAR", value: "VALLEDUPAR" },
+    {display: "AGUACHICA", value: "AGUACHICA" }
+];
+
+var CHOCO = [
+    {display: "QUIBDO", value: "QUIBDO" }
+];
+
+var CORDOBA = [
+    {display: "MONTERIA", value: "MONTERIA" },
+    {display: "CERETE", value: "CERETE" },
+    {display: "MONTELIBANO", value: "MONTELIBANO" },
+    {display: "PLANETA RICA", value: "PLANETA RICA" },
+    {display: "SAN BERNARDO DEL VIENTO", value: "SAN BERNARDO DEL VIENTO" },
+    {display: "TIERRALTA", value: "TIERRALTA" }
+];
+
+var CUNDINAMARCA = [
+    {display: "BOGOTA", value: "BOGOTA" },
+    {display: "FACATATIVA", value: "FACATATIVA" },
+    {display: "FUSAGASUGA", value: "FUSAGASUGA" },
+    {display: "GIRARDOT", value: "GIRARDOT" },
+    {display: "SOACHA", value: "SOACHA" }
+];
+
 $("#state").change(function(){
 
 var select = $("#state option:selected").val();
@@ -71,9 +110,37 @@ case "CALDAS":
     city(CALDAS);
 break;
 
+case "CAQUETA":
+    city(CAQUETA);
+break;
+
+case "CASANARE":
+    city(CASANARE);
+break;
+
+case "CAUCA":
+    city(CAUCA);
+break;
+
+case "CESAR":
+    city(CESAR);
+break;
+
+case "CHOCO":
+    city(CHOCO);
+break;
+
+case "CORDOBA":
+    city(CORDOBA);
+break;
+
+case "CUNDINAMARCA":
+    city(CUNDINAMARCA);
+break;
+
 default:
     $("#city").empty();
-    $("#city").append("<option>--Seleccionar ciudad--</option>");
+    $("#city").append("<option>*Seleccionar ciudad</option>");
 break;
 }
 });
@@ -81,7 +148,7 @@ break;
 //Function To List out Cities in Second Select tags
 function city(arr){
     $("#city").empty();//To reset cities
-    $("#city").append("<option>--Seleccionar ciudad--</option>");
+    $("#city").append("<option>*Seleccionar Ciudad</option>");
     $(arr).each(function(i){//to list cities
         $("#city").append("<option value=\""+arr[i].value+"\">"+arr[i].display+"</option>")
     });
