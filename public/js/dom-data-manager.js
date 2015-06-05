c578c8169bab8a418ae3c8c2e0b75f9598db6e7d
unknown <dorian.lopez@desarrollo1.sigmamovil.local>
2015-06-05 14:28:33 -0500

    Ajuste

--- public/js/dom-data-manager.js
+++ public/js/dom-data-manager.js
@@ -50,7 +50,7 @@ DomManager.prototype.refreshTable = function() {
                             <td>' + this.rows[i].client + '</td>\n\
                             <td>' + this.rows[i].battery + '%</td>\n\
                             <td>\n\
-                                <a href="' + url + '/map/' + this.rows[i].idUser + '">' + this.rows[i].location + '</a><br />\n\
+                                <a href="' + url + '/map/' + this.rows[i].idVisit + '">' + this.rows[i].location + '</a><br />\n\
                                 <a href="' + url + '/maphistory/' + this.rows[i].idUser + '">Ver historial</a>\n\
                             </td>\n\
                         </tr>');
