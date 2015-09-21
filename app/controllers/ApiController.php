b004f53eb31dd0a16271c514039b7196acb3d34a
unknown <william.montiel@liderdesarrollo.sigmamovil.local>
2015-09-21 10:57:03 -0500

    no message

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -29,10 +29,10 @@ class ApiController extends \Phalcon\Mvc\Controller
 				$obj->client = $value->client->name;
 				$obj->start = date('d/M/Y H:s', $value->visit->start);
 				$obj->end = date('d/M/Y H:s', $value->visit->end);
-				$time1 = date_create(\date('Y-m-d H:i:s', $row['end']));
-                $time2 = date_create(\date('Y-m-d H:i:s', $row['start']));
+				$time1 = date_create(\date('Y-m-d H:i:s', $value->visit->start));
+                $time2 = date_create(\date('Y-m-d H:i:s', $value->visit->end));
                 $interval = date_diff($time1, $time2);
-                $obj->elapsed = $interval->format("%a día(s) %h:%i:%s");
+                $obj->elapsed = $interval->format("%a día(s) %H:%I:%S");
 				$obj->iLatitude = $value->visit->latitude;
 				$obj->iLongitude = $value->visit->longitude;
 				$obj->fLongitude = $value->visit->finalLongitude;
